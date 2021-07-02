f6b1543e51a72216fa1cdae40ce21c4d8269111d
Julius-R <julius.robinson95@yahoo.com>
2021-07-01 18:59:38 -0600

    made changes to index.js and app.js files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import store from "./redux/index";
-import { Provider, useDispatch } from "react-redux";
 
 export default function App() {
-  const dispatch = useDispatch();
   return (
-    <Provider store={store}>
-      {console.log(store.getState())}
+   <>
       <div>Test</div>
-    </Provider>
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import store from "./redux/index";
+import { Provider } from "react-redux";
 import "./styles/index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>, document.getElementById("app"));
