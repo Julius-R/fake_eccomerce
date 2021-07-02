adc14e68ae8359d1295d04a4308044d5d87f6e55
Julius-R <julius.robinson95@yahoo.com>
2021-07-01 19:18:22 -0600

    testing out info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const cart = useSelector((state) => state);
   return (
-   <>
-      <div>Test</div>
+    <>
+      <div>Cart Count: {cart.qty}</div>
     </>
   );
 }
